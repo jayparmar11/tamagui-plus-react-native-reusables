feae3dbaefbcda4fb021c125968b52a692baba72
Jay Parmar <jaysparmar303@gmail.com>
2026-01-20 21:43:57 +0530

    fix axios client

--- packages/api/axios-client.ts
+++ packages/api/axios-client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
 export const AXIOS_INSTANCE = axios.create({
-  baseURL: 'http://192.168.1.12:5000',
+  baseURL: 'http://localhost:5000',
 })
 
 export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
